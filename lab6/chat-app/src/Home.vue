32653938709aa860f9f558b7173005b7dcf31c94
Mike Whitley <mkw60@uclive.ac.nz>
2020-05-18 14:05:17 +1200

    Initial commit

---
+++ lab6/chat-app/src/Home.vue
@@ -0,0 +1,35 @@
+<template>
+    <dev> Hello
+      <button v-on:click="MovePage">Users page</button>
+
+    </dev>
+
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        error: "",
+        errorFlag: false,
+      }
+    },
+    mounted: function() {
+      this.getUsers();
+    },
+    methods: {
+
+      MovePage: function(){
+        this.$router.push('/users').catch((err) => {
+        })
+      }
+
+    }
+  }
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ lab6/chat-app/src/main.js
@@ -0,0 +1,38 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Home from './Home.vue'
+import Users from './Users.vue'
+import axios from 'axios';
+import VueAxios from "vue-axios";
+import VueRouter from 'vue-router';
+Vue.use(VueAxios, axios)
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    component: Home
+  },
+  {
+    path: "/users/:userId", //this is the URL
+    name: "user", // this is the name we use to call it inside the function
+    component: Users //this is the Vue file name
+  },
+  {
+    path: "/users",
+    name:'users', // this is the name when we say <router-link :to="{name: 'users'}">Back to Users</router-link> that we return to
+    component: Users
+  },
+];
+
+const router = new VueRouter({
+  routes: routes,
+  mode: 'history'
+})
+
+
+new Vue({
+  el: '#app',
+  router: router,
+  render: h => h(App)
+})
